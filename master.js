f8fe4d26005d10f1713c2daf23dd6a3740fcb946
omaralhaded199 <93614272+omaralhaded199@users.noreply.github.com>
2021-11-02 12:04:06 -0700

    Create master.js

---
+++ master.js
@@ -0,0 +1,385 @@
+$(`
+<a id="des3" href="#" style="border: 1px solid white;border-radius: 0px 15px 0px 0px!important;width:70px;margin:-145px 2px 2px;" class="fl mini  fa   fa-phone btn btn-warning"> الأداره </a>
+<a id="des3" href="#" style="border: 1px solid white;border-radius: 0px!important;width:70px;margin:-116px 2px 2px;" class="fl mini  fa  fa-paint-brush btn btn-danger"> التطبيق </a>
+<a id="des3" href="#" style="border: 1px solid white;border-radius: 0px!important;width:70px;margin:-87px 2px 2px;" class="fl mini  fa   fa-star btn btn-success"> اشتراك </a>
+<a id="des3" href="#" style="border: 1px solid white;border-radius: 0px!important;width:70px;margin:-58px 2px 2px;" class="fl mini  fa   fa-image btn btn-warning"> رمزيات </a>
+<a id="des3" href="#" style="border: 1px solid white;    border-radius: 0px 0px 15px 0px!important;width:70px;margin:-29px 2px 2px;" class="fl mini  fa fa-heartbeat btn btn-info"> شات </a>
+`).insertBefore('.nav-tabs');
+
+
+
+$(`<style>
+
+
+.uzr.fl.corner.borderg {
+    margin-bottom: 0px!important;
+}
+
+
+button.ssss.btn.fr.btn-warning.fa.fa-paint-brush {
+display: none;
+}
+.hand.nosel.fl.uzr.border {
+    border-radius: 5px!important;
+}
+
+textarea.form-control.borderg.primaryborder.corner.fr.ulikeins {
+    font-size: 15px!important;
+    color: #060000;
+    padding: 4px!important;
+}
+
+
+
+textarea.form-control.borderg.primaryborder.corner.fr.u-topic {
+    font-size: 15px!important;
+    color: #060000;
+    padding: 4px!important;
+}
+
+
+</style>
+`).insertBefore('body');
+
+$('.break.light').css({"background-image":"url(https://chatnsh.com/sico/z1fjh0cgo710.png)"});
+
+$("[data-target=#wall]").attr('title','  ♥️ مســـاحه حــره لأبــدآعاتكــم ♥️           ')
+
+
+$(".fa-gear").text("آلاعدآدات").css("width","");
+
+
+
+
+
+$(` <center><div width="99%" id="asim7"><font style="background-color: #dbf6f1;border-radius: 15px 15px 15px 15px;font-family: 'jazeera',FontAwesome;padding: 2px 10px 2px 10px;margin: 7px 10px 7px 10px;" > سوبر الحائط  (قـــريبـــآ) </font></div><center>`).insertBefore('div#d2bc');
+
+
+$(`<div><marquee direction="right" width="100%" id="des0" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="4"> اهلا وسهلا بكم في شات <font color="#357524" class="des2"> أحبـــــك للجوال </font>افضل حضور الاسبوع<font class="des2" color="#800000">قـــريـــبأ </font>مبدع الشات <font class="des2" color="#223366">  قـــريـــبأ </font>افضل روم لهذا الاسبوع<font class="des2" color="#ff00ff"> قـــريـــبأ    </font>افضل سوبر هذا الآسبوع  <font class="des2" color="#5CAD9A">قـــريـــبأ </font>مبدع الحائط لهذا الاسبوع<font class="des2" color="#800000"> قـــريـــبأ </font>مع تمنياتنا للجميع باطيب الاوقات </marquee></div>`).insertBefore('.lonline.light.break');
+
+
+$(`${'<form name="jump">\x0D\x0A<div class="MyColors">\x0D\x0A					<map id="ColorsMap" name="ColorsMap">\x0D\x0A					<area alt="" coords="275, 0, 298, 25" href="javascript:chooseStyle(\'none\',60)" shape="rect" title="1">\x0D\x0A					<area alt="" coords="244, 0, 266, 25" href="javascript:chooseStyle(\'Co2\',60)" shape="rect" title="2">\x0D\x0A					<area alt="" coords="212, 0, 233, 25" href="javascript:chooseStyle(\'Co3\',60)" shape="rect" title="3">\x0D\x0A					<area alt="" coords="179, 0, 201, 25" href="javascript:chooseStyle(\'Co4\',60)" shape="rect" title="4">\x0D\x0A					<area alt="" coords="148, 0, 170, 25" href="javascript:chooseStyle(\'Co5\',60)" shape="rect" title="5">\x0D\x0A					<area alt="" coords="115, 0, 140, 25" href="javascript:chooseStyle(\'Co6\',60)" shape="rect" title="6">\x0D\x0A					<area alt="" coords="105, 0, 85, 25" href="javascript:chooseStyle(\'Co7\',60)" shape="rect" title="7">\x0D\x0A					<area alt="" coords="75, 0, 52, 25" href="javascript:chooseStyle(\'Co8\',60)" shape="rect" title="8">\x0D\x0A					</map>\x0D\x0A			        <img alt="" height="40" src="https://1.top4top.net/p_1388gqx1c1.gif" width="300" usemap="#ColorsMap"></div>\x0D\x0A</form>\x0D\x0A\x0D\x0A\x0D\x0A\x0D\x0A\x0D\x0A\x0D\x0A'}`)['insertBefore']('.nav-tabs');
+
+
+$('<div class=\'fr borderg\' style=\'padding:2px;background-color:white;float:left;\'> استايلات و ارشفة :<a href=\'http://www.1des.net/\'>ون ديزاين</a></div>')['insertAfter']($('.loginstat'));
+
+
+$('.checkbox label')['html'](`${'<input id="stealth" type="checkbox" value="" maxlength="12"><i class="fa fa-eye-slash" aria-hidden="true">'}`);
+
+
+$('.nav-tabs')['addClass']('fl')['css']('width', '100%');
+
+
+$(`<link rel="stylesheet" type="text/css" href="">
+<link href="https://rawcdn.githack.com/omaralhaded199/OmarAL-Haded/b4813fb933c925957c01954952ebcdc382c6a8a8/1.css" rel="stylesheet" type="text/css">
+<link href="https://rawcdn.githack.com/omaralhaded199/OmarAL-Haded/b4813fb933c925957c01954952ebcdc382c6a8a8/2.css" media="screen" rel="alternate stylesheet" title="Co2" type="text/css">
+<link href="https://rawcdn.githack.com/omaralhaded199/OmarAL-Haded/b4813fb933c925957c01954952ebcdc382c6a8a8/3.css" media="screen" rel="alternate stylesheet" title="Co3" type="text/css">
+<link href="https://rawcdn.githack.com/omaralhaded199/OmarAL-Haded/b4813fb933c925957c01954952ebcdc382c6a8a8/4.css" media="screen" rel="alternate stylesheet" title="Co4" type="text/css">
+<link href="https://rawcdn.githack.com/omaralhaded199/OmarAL-Haded/b4813fb933c925957c01954952ebcdc382c6a8a8/5.css" media="screen" rel="alternate stylesheet" title="Co5" type="text/css">
+<link href="https://rawcdn.githack.com/omaralhaded199/OmarAL-Haded/b4813fb933c925957c01954952ebcdc382c6a8a8/6.css" media="screen" rel="alternate stylesheet" title="Co6" type="text/css">
+<link href="https://rawcdn.githack.com/omaralhaded199/OmarAL-Haded/b4813fb933c925957c01954952ebcdc382c6a8a8/7.css" media="screen" rel="alternate stylesheet" title="Co7" type="text/css">
+<link href="https://rawcdn.githack.com/omaralhaded199/OmarAL-Haded/b4813fb933c925957c01954952ebcdc382c6a8a8/8.css" media="screen" rel="alternate stylesheet" title="Co8" type="text/css">
+<style>
+.MyColors {
+    position: absolute;
+	transform:matrix(1, 0, 0, 1, 85, -1);
+	margin-top: 2px;
+    width: 300px;
+    height: 60px;
+    float: left;
+    background: transparent url(https://chatnsh.com/sico/z1fjh0cdgp10.png) no-repeat scroll right top;
+    text-align: left;
+}
+
+
+
+img.fitimg {
+    border-radius: 25px;
+}
+
+
+div#users img.fl.ustat {
+    border-radius: 37px 37px 5px 37px;
+    margin: 0;
+    margin-right: -38.5px;
+    margin-top: -1px;
+    width: 39px!important;
+    height: 39px!important;
+}
+
+
+fl.u-pic {
+border-radius: 20px !important;
+}
+
+button.btn.fr.btn-success.fa.fa-refresh {
+display: none;
+}
+
+a.label.label-primary.fl img.fl {display: none;}
+
+a.label.label-primary.fl {
+    border-radius: 25px!important;
+    margin: 2px -252px;
+    padding: 4px!important;
+    margin-bottom: 2px;
+    text-align: right!important;
+    position: absolute;
+}
+
+div#l2 br,div#l3 br {
+    display: none
+}
+
+#des22 {
+text-align: center!important;
+font-size: 100%!important;
+}
+label.fl.label.loginstat {
+background-color: transparent!important;
+position: absolute;
+top: 0;
+margin-top: 4px;
+left: 0;
+}
+.fr.borderg, .fr.borderg a {
+    font-size: 10px!important;
+    padding: 0 5px!important;
+    position: relative;
+    color: #ffffff;
+}
+.fr.borderg {
+    border: 0px;
+    margin-top: 0;
+    height: 0;
+
+}
+span.s1.fa.fa-user.label.badgex.label-as-badge.label-success {
+padding: 0px 3px 2px;
+margin-right: 5px;
+}
+label.label.label-primary.mini.fl {
+padding: 1px;
+border-radius: 15px!important;
+}
+
+
+
+
+div#l1 input#u1,div#l3 button {
+    width: 99%;
+    margin-left: 1px
+}
+
+div#l2 button,div#l3 button {
+    border-radius: 24px!important;
+    height: 27px;
+	padding: 0px 5px;
+}
+
+div#l1 input#u1 {
+    border-radius: 15px;
+    border: 1px solid #e1e0e0;
+    margin-top: 3px;
+    text-align: center
+}
+
+div#l2 input#pass1,div#l2 input#u2,div#l3 input#pass2,div#l3 input#u3 {
+    width: 49%;
+    padding: 0!important;
+    float: right;
+    margin: 2px 5px 2px 0
+}
+
+div#l2 input#u2,div#l3 input#u3 {
+    margin-left: -6px;
+    border-radius: 15px;
+    border: 1px solid #e1e0e0;
+    text-align: center
+}
+
+div#l2 input#pass1,div#l3 input#pass2 {
+    border-radius: 15px;
+    border: 1px solid #e1e0e0;
+    text-align: center
+}
+
+div#l2,div#l3 {
+}
+
+div#l1 {
+    margin: 0 1px -18px;
+}
+
+div#l1,div#l2,div#l3 {
+    border-bottom: 2px solid;
+    margin-bottom: 2px;
+    border-top: 2px solid;
+    margin-top: 0;
+    border-color: #bababa;
+    border-radius: 15px;
+    height: 69px
+}
+
+div#l1 button {
+
+    border-radius: 24px!important;
+    width: 99%;
+    height: 27px;
+    padding: 0px 5px;
+}
+
+div#l2 button {
+    width: 70%
+}
+
+ul.nav.nav-tabs>li>a {
+    border: none;
+    text-align: center;
+    border-radius: 3px;
+    display: initial;
+    width: 80%;
+    float: left;
+    padding: 0!important;
+}
+
+ul.nav.nav-tabs>li.active>a {
+    border: none;
+    border-radius: 3px;
+}
+
+ul.nav.nav-tabs>li {
+    width: 32%;
+    margin: 0 5px 0 0;
+}
+
+ul.nav.nav-tabs>li>.fa-user-plus:before,ul.nav.nav-tabs>li>.fa-user:before,ul.nav.nav-tabs>li>.fa-user:before {
+    float: right;
+    margin-right: -26%;
+    padding: 0 2px 1px;
+    border-radius: 3px;
+    width: 23px;
+    text-align: center;
+}
+
+
+
+.nav>li>a:focus,.nav>li>a:hover {
+    color: #9abff9;
+}
+
+ul.nav.nav-tabs {
+    margin-top: 2px;
+    float: right;
+    width: 100%;
+    padding: 3px 1px;
+}
+
+
+
+
+
+
+
+#des0 {
+    transform: matrix(1, 0, 0, 1, 126, -124);
+    border-radius: 15px 15px 15px 15px;
+    position: absolute;
+    width: 46% !important;
+    height: 19px;
+    left: -50px;
+    font-size: 14px !important;
+    color: #fff;
+    padding: 0px 0 !important;
+}
+
+
+.des2 {
+    background-color: #fff;
+    border-radius: 0;
+    padding: 2px 10px;
+    margin: 3px 8px!important;
+	border-radius: 15px!important;
+}
+</style>`)['insertBefore']('body')
+
+
+
+var manual_or_random = 'manual';
+var randomsetting = '3 days';
+
+function getCookie(_0x2915x4) { var _0x2915x5 = new RegExp(_0x2915x4 + '=[^;]+', 'i'); if (document['cookie']['match'](_0x2915x5)) { return document['cookie']['match'](_0x2915x5)[0]['split']('=')[1] }; return null }
+
+function setCookie(_0x2915x7, _0x2915x8, _0x2915x9) { var _0x2915xa = new Date(); var _0x2915xb = (typeof _0x2915x9 != 'undefined') ? _0x2915xa['setDate'](_0x2915xa['getDate']() + parseInt(_0x2915x9)) : _0x2915xa['setDate'](_0x2915xa['getDate']() - 5);
+    document['cookie'] = _0x2915x7 + '=' + _0x2915x8 + '; expires=' + _0x2915xa['toGMTString']() + '; path=/' }
+
+function deleteCookie(_0x2915x7) { setCookie(_0x2915x7, 'moot') }
+
+function setStylesheet(_0x2915xe, _0x2915xf) { var _0x2915x10, _0x2915x11, _0x2915x12 = ['']; for (_0x2915x10 = 0;
+        (_0x2915x11 = document['getElementsByTagName']('link')[_0x2915x10]); _0x2915x10++) { if (_0x2915x11['getAttribute']('rel')['toLowerCase']() == 'alternate stylesheet' && _0x2915x11['getAttribute']('title')) { _0x2915x11['disabled'] = true;
+            _0x2915x12['push'](_0x2915x11); if (_0x2915x11['getAttribute']('title') == _0x2915xe) { _0x2915x11['disabled'] = false } } }; if (typeof _0x2915xf != 'undefined') { var _0x2915x13 = Math['floor'](Math['random']() * _0x2915x12['length']);
+        _0x2915x12[_0x2915x13]['disabled'] = false }; return (typeof _0x2915xf != 'undefined' && _0x2915x12[_0x2915x13] != '') ? _0x2915x12[_0x2915x13]['getAttribute']('title') : '' }
+
+function chooseStyle(_0x2915x15, _0x2915x9) { if (document['getElementById']) { setStylesheet(_0x2915x15);
+        setCookie('mysheet', _0x2915x15, _0x2915x9) } }
+
+function indicateSelected(_0x2915x17) { if (selectedtitle != null && (_0x2915x17['type'] == undefined || _0x2915x17['type'] == 'select-one')) { var _0x2915x17 = (_0x2915x17['type'] == 'select-one') ? _0x2915x17['options'] : _0x2915x17; for (var _0x2915x10 = 0; _0x2915x10 < _0x2915x17['length']; _0x2915x10++) { if (_0x2915x17[_0x2915x10]['value'] == selectedtitle) { if (_0x2915x17[_0x2915x10]['tagName'] == 'OPTION') { _0x2915x17[_0x2915x10]['selected'] = true } else { _0x2915x17[_0x2915x10]['checked'] = true }; break } } } }
+if (manual_or_random == 'manual') { var selectedtitle = getCookie('mysheet'); if (document['getElementById'] && selectedtitle != null) { setStylesheet(selectedtitle) } } else { if (manual_or_random == 'random') { if (randomsetting == 'eachtime') { setStylesheet('', 'random') } else { if (randomsetting == 'sessiononly') { if (getCookie('mysheet_s') == null) { document['cookie'] = 'mysheet_s=' + setStylesheet('', 'random') + '; path=/' } else { setStylesheet(getCookie('mysheet_s')) } } else { if (randomsetting['search'](/^[1-9]+ days/i) != -1) { if (getCookie('mysheet_r') == null || parseInt(getCookie('mysheet_r_days')) != parseInt(randomsetting)) { setCookie('mysheet_r', setStylesheet('', 'random'), parseInt(randomsetting));
+                        setCookie('mysheet_r_days', randomsetting, parseInt(randomsetting)) } else { setStylesheet(getCookie('mysheet_r')) } } } } } }
+
+
+if(location.protocol!='https:'){location.protocol='https:';}
+bct=100;msgt=100;
+var s=document.styleSheets[2].rules[20].style;s["width"]='auto';s["max-width"]='';s["max-height"]='20px';
+$("div#tlogins input").attr("maxlength",13),$("textarea#tbox.fl.corner.tbox.filw").attr("maxlength",1000);
+$(`<center><div><div width="99.5%" id="" /font/div/div><center>`).insertBefore('#d2');
+$('.u-msg.dots').removeClass('dots');
+$(".ubnr").remove();
+$(".ulikec.border").text('حفظ اللايكات');
+$(".upower.border").text('حفظ الصلاحيات'); 
+$(".uroomz.border").text('نقــل للغرفه '); 
+$(`<span class="rating-full-star"></span>`).insertBefore('.ulike.borderg');
+
+$(`<div class="youtubeSearch" style="position: relative;float: right;width: 100%;top: 0;"> <div class="youtubeLoad" style="display: none;text-align: center;position: absolute;right: 0;left: 0;background-color: #ffffff80;"> <img style="margin-top: -9px;width: 60px;" src="imgs/icon.gif"> </div> <div style="display: flex;background-color: white;padding: 2px 0;border-bottom: 1px solid;"> <i onclick="send('youtube', {'search':$('.youtubeVal').val()});$('.youtubeLoad').show()" style="float: left;font-size: 24px !important;color: #6d6b6b;font-weight: 100!important;margin: 3px;" class="fa fa-search" aria-hidden="true"></i> <input style="text-align: center;float: right;" type="text" class="form-control youtubeVal" placeholder="البحث في يوتيوب"> <i style="float: right;font-size: 30px !important;color: red;font-weight: 100!important;margin: 3px;" class="fa fa-youtube" aria-hidden="true"></i> </div> <div style="display: none;" class="vieYoutube"> <img style="min-height: 60px;width: 100%;float: left;" alt=""> <span style="margin-top: -56px;float: left;background-color: #ffffffa1;color: red;padding: 2px 0;text-align: center;width: 100%;" class="dots youtubeTitl"></span> <button style="margin: -28px 2px 0px; width: 48%; text-align: center; box-shadow: rgb(34, 34, 34) 0px 0px 10px;" class="youtubeSend fa fa-send fr btn btn-primary">ارسال الى الحائط</button> <button onclick="$('.vieYoutube').hide();" style="margin: -28px 2px 0px; width: 48%; text-align: center; box-shadow: rgb(34, 34, 34) 0px 0px 10px;" class="youtubeCloos fa fl btn btn-primary">الغاء</button> </div> `).insertBefore("#d2bc");
+
+if(location.protocol!='https:') {location.protocol='https:';}
+
+if (getUrlParameter('enter') != null) {location.href='/';}
+
+AddMsg_=AddMsg;AddMsg=function(wid,data){data.msg=data.msg.replace(/\u102C/g,"");return AddMsg_(wid,data);}
+
+$(".fa-gear").text("الاعدادات").css("width","");
+
+
+$("div#tlogins button.btn.btn-primary").click(function(){
+var myVar = setInterval(function(){ var usmsgw = $(".pmsgc").length;if(usmsgw > 0){
+   $(`
+    <div class="uzr fl corner borderg mm" style="border-radius:5px;margin-bottom:-2px;width:99.5%;padding:0px; background-color:white;">
+      <img style="width: 36px; height: 38px; margin-left: 1px; margin-top: 1px; background-image: url(https://chatnsh.com/room.png?1);" class="fl fitimg hand u-pic    ">
+      <div class="uzr fl" style="padding:0px;width:80%">
+        <div style="padding:0px;width:100%;" class="fl">
+          <img class="fl u-ico" alt="" src="">
+          <span style="padding: 1px 8px; margin-top: 2px; display: block; max-width: 82%; border-radius: 3px; color: rgb(199, 103, 48);" class="corner nosel u-topic dots fl hand">ادارة شات آحبــك ترحب  ~  بكـم</span>
+        </div>
+        <br>
+        <div style="padding: 0px; width: 100%; color: rgb(60, 0, 255);" class=" u-msg   break  fl">
+        <div style="padding: 0px;width: 100%;color: rgb(60, 0, 255);text-align: left;display: block;margin-top: -17px;" class=" u-msg   break  fl"> مرحباً بك  <h1 style="display: inline-block;color: red;">`+ getuser(myid).topic+ `</h1> نقدّر تواجدك معنا، ونتمنى لك يوماً سعيداً إن شاء الله </div>
+      </div>
+    </div></div>
+`).appendTo('div#d2')
+clearInterval(myVar);
+}else{console.log(usmsgw)} }, 2000);
+})
+$(`<style>
+div#d2bc .uzr.fl.corner.borderg {
+    border-radius: 9px !important;
+    margin: 3px 1px -1px 2px !important;
+    border: 5px solid transparent;
+    padding: 15px;
+    -webkit-border-image: url() 30 round; /* Safari 3.1-5 */
+    -o-border-image: url() 30 round; /* Opera 11-12.1 */
+    border-image: url() 30 round;
+}
+</style>`).insertBefore('body');
+
+$(`<div style=" color: red;text-align: center;margin-bottom: 10px; font-family:'jazeera-light', Fontawesome">للترحيب والفعاليات</div>`).insertBefore('span.fl.fa.fa-send');
